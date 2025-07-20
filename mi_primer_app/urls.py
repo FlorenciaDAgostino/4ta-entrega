65fcee265c23e12f118cce5b9290f0b335cd3408
Florencia DAgostino <dagostinoflorencia@gmail.com>
2025-07-20 12:04:31 -0300

    se agrega el eliminar y listar

--- mi_primer_app/urls.py
+++ mi_primer_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from .views import (saludo, saludo_con_template, crear_familiar,
                     inicio, crear_curso, crear_estudiante, buscar_cursos, cursos, crear_profesores, crear_televisores,
-                    CelularesCreateView, CelularesListView, HeladerasCreateView, HeladerasListView, LavarropasCreateViews,LavarropasListView)
+                    CelularesCreateView, CelularesListView, CelularesDeleteView, CelularesDetailView, CelularesUpdateView, HeladerasCreateView, HeladerasListView, HeladerasDeleteView, HeladerasDetailView, HeladerasUpdateView, LavarropasCreateViews,LavarropasListView, LavarropasDeleteView, LavarropasDetailView, LavarropasUpdateView)
 
 urlpatterns = [
     path('', inicio, name= 'inicio'),
@@ -19,11 +19,19 @@ urlpatterns = [
     # urls con vistas basadas en clase
     path('listar-celulares/', CelularesListView.as_view(), name= 'listar-celulares'),
     path('crear-celulares/', CelularesCreateView.as_view(), name= 'crear-celulares'),
+    path('detalle-celulares/<int:pk>', CelularesDetailView.as_view(), name='detalle-celulares'),
+    path('editar/<int:pk>/', CelularesUpdateView.as_view(), name='editar-celulares'),
+    path('eliminar/<int:pk>/', CelularesDeleteView.as_view(), name='eliminar-celulares'),
     path('listar-heladeras/', HeladerasListView.as_view(), name= 'listar-heladeras'),
     path('crear-heladeras/', HeladerasCreateView.as_view(), name= 'crear-heladeras'),
+    path('detalle-heladeras/<int:pk>', HeladerasDetailView.as_view(), name='detalle-heladeras'),
+    path('editar/<int:pk>/', HeladerasUpdateView.as_view(), name='editar-heladeras'),
+    path('eliminar/<int:pk>/', HeladerasDeleteView.as_view(), name='eliminar-heladeras'),
     path('listar-lavarropas/', LavarropasListView.as_view(), name= 'listar-lavarropas'),
     path('crear-lavarropas/', LavarropasCreateViews.as_view(), name= 'crear-lavarropas'),
-
+    path('detalle-lavarropas/<int:pk>', LavarropasDetailView.as_view(), name='detalle-lavarropas'),
+    path('editar/<int:pk>/', LavarropasUpdateView.as_view(), name='editar-lavarropas'),
+    path('eliminar/<int:pk>/', LavarropasDeleteView.as_view(), name='eliminar-lavarropas'),
 ]
     
     
